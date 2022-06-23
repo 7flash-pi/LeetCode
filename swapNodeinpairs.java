f104bb12bfaf52f5cf204716e90afb10f7b1b8a2
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-06-23 19:39:54 +0530

    final

---
+++ swapNodeinpairs.java
@@ -0,0 +1,29 @@
+//Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode *temp=head;
+        if(head==NULL)
+            return head;
+        while(temp!=NULL && temp->next!=NULL)
+        {
+            int k=temp->val;
+            temp->val=temp->next->val;
+            temp->next->val=k;
+            temp=temp->next->next;
+        }
+        return head;
+        
+    }
+}
+;
