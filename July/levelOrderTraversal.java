7aca0df1bfaa94f001983f399fa393f20d6c77a1
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-07-13 14:30:39 +0530

    13072022

---
+++ July/levelOrderTraversal.java
@@ -0,0 +1,44 @@
+/*
+ * Given the root of a binary tree, return the level order traversal of its
+ * nodes' values. (i.e., from left to right, level by level).
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int qlen = queue.size();
+            List<Integer> row = new ArrayList<>();
+            for (int i = 0; i < qlen; i++) {
+                TreeNode curr = queue.poll();
+                row.add(curr.val);
+                if (curr.left != null)
+                    queue.add(curr.left);
+                if (curr.right != null)
+                    queue.add(curr.right);
+            }
+            ans.add(row);
+        }
+        return ans;
+    }
+}
